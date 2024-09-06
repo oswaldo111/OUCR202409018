674951a991a0ee510f25a090587af1c42a622563
oswaldo chilin <oswaldochilin49@gmail.com>
2024-09-06 15:56:10 -0600

    se arreglaron errores en las redirecciones

--- CRM.AppWebBlazor/obj/Debug/net6.0/CRM.AppWebBlazor.AssemblyInfo.cs
+++ CRM.AppWebBlazor/obj/Debug/net6.0/CRM.AppWebBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM.AppWebBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9197807e8bc1680685a52f11b453f904c1898a98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c205570fdc4fb17d3bf7e219e3b61ad68c8f8837")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM.AppWebBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM.AppWebBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
